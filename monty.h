1f91cc4549fe77db0f0b449db6530c125945367d
Zamzamke <zamzamchelimo@gmail.com>
2024-01-18 14:09:05 -0800

    created the header file

---
+++ monty.h
@@ -0,0 +1,35 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack
+ * @n: integer
+ * @prev: pointer  to the previous element of the stack
+ * @next: pointer  to the next element of the stack
+ */
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - the opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
